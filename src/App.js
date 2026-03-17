ed2b2b48d2005980c71b55df62eb65e5414e736b
Bonnie_chris <bonifacekalama07@gmail.com>
2026-03-17 10:52:49 +0300

    soko projacter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Signup from './Components/Signup';
 import Signin from './Components/Signin';
 import Addproduct from './Components/Addproduct';
+import Getproduct from './Components/Getproduct';
 function App() {
   return (
     <BrowserRouter>
@@ -13,15 +14,18 @@ function App() {
           <h1>Welcome to sokogarden</h1>
         </header>
         <nav>
-          <Link to="/Signup">Sign up</Link>
-          <Link to="/Signin">Sign in</Link>
-          <Link to="/addproduct">Add product</Link>
+          <Link to="/Signup" className='btn btn-warning m-2'>Sign up</Link>
+          <Link to="/Signin" className='btn btn-danger m-2'>Sign in</Link>
+          <Link to="/addproduct" className='btn btn-success m-2'>Add product</Link>
+          <Link to="/" className=' btn btn-primary m-2'>Get product</Link>
 
         </nav>
         <Routes>
+          <Route path='/' element={<Getproduct />} />
           <Route path='/Signup' element={<Signup />} />
           <Route path='/Signin' element={<Signin />} />
           <Route path='/addproduct' element={<Addproduct />} />
+
 
         </Routes>
       </div>
---
+++ src/Components/Getproduct.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+const Getproduct = () => {
+    // declare our states here 
+    const [loading, setLoading] = useState("")
+    const [product, setproducts] = useState([])
+    const [error, setError] = useState("")
+    // function to get product 
+    const getproduct = async () => {
+        setLoading("Please wait...")
+
+        try {
+            const response = await axios.get("http://bonifacekifaru.alwaysdata.net/api/getproducts")
+            setproducts(response.data)
+            setLoading("")
+        }
+        catch (error) { }
+    }
+    // call our function
+    useEffect(() => {
+        getproduct()
+    }, [])
+    console.log(product);
+    const imagepath = "http://bonifacekifaru.alwaysdata.net/static/images/"
+
+
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <h1 className='text-success'>Available products</h1>
+                {/* bind the states  */}
+                <i className='text-primary'>{loading}</i>
+                <i className='text-danger'>{error}</i>
+
+                {product.map(singleproduct => (
+                    <div className="col-md-4 card shadow   mb-3">
+                        {/* card body goes here  */}
+                        <div className="card-body">
+                            {/* product name goes here  */}
+                            <h1 className='text-primary'>{singleproduct.product_name}</h1>
+                            {/* image goes here  */}
+                            <img src={imagepath + singleproduct.product_photo} alt="" />
+                            {/* product desctription goes here  */}
+                            <p>{singleproduct.product_description}</p>
+                            {/* product cost goes here  */}
+                            <b className='text-warning'>{singleproduct.product_cost}</b><br />
+                            {/* purchase now button  */}
+                            <button className='btn btn-success text-danger w-100'>Purchase now</button>
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Getproduct
